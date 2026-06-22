bc332986f6e8389ea6c3f5891ab3b9739e837063
Nathan Patzer <nate.patzer@gmail.com>
2026-06-22 18:36:25 -0500

    Add movement functionality to Note and refine resizing logic

--- src/objects/note.js
+++ src/objects/note.js
@@ -14,6 +14,9 @@ class Note extends Container {
         this.pointerdown = false;
         this.resizingRight = false;
         this.resizingLeft = false;
+        this.moving = false;
+        this.moveStartX = undefined;
+        this.moveStartY = undefined;
         const rect = new Graphics();
         rect.rect(0, 0, this.noteWidth, this.noteHeight);
         rect.fill(this.color);
@@ -60,7 +63,7 @@ class Note extends Container {
         let resizeStartX = 0;
         let resizeStartWidth = 0;
         const EDGE_THRESHOLD = 10;
-
+        this.moveStartX = 0;
         this.on('globalpointermove', (e) => {
             const local = this.toLocal(e.global);
             
@@ -69,7 +72,7 @@ class Note extends Container {
             this.cursor = (nearRightEdge || nearLeftEdge || this.resizingRight || this.resizingLeft) ? 'ew-resize' : 'grab';
 
             if (this.pointerdown) {
-                if (nearRightEdge || this.resizingRight) {
+                if ((nearRightEdge || this.resizingRight) && !this.resizingLeft && !this.moving) {
                     this.resizingRight = true;
                     const dx = local.x;
                     const snappedDx = Math.round(dx / this.quantizedNoteWidth) * this.quantizedNoteWidth;
@@ -79,19 +82,42 @@ class Note extends Container {
                         this._reDraw(newWidth);
                     }
                 }
-            else if (this.resizingLeft || nearLeftEdge) {
-                this.resizingLeft = true;
-                const dx = this.noteWidth - local.x;
-                const snappedDx = Math.round(dx / this.quantizedNoteWidth) * this.quantizedNoteWidth;
-                const newWidth = Math.max(this.quantizedNoteWidth, snappedDx);
+                else if ((this.resizingLeft || nearLeftEdge) && !this.resizingRight && !this.moving) {
+                    this.resizingLeft = true;
+                    const dx = this.noteWidth - local.x;
+                    const snappedDx = Math.round(dx / this.quantizedNoteWidth) * this.quantizedNoteWidth;
+                    const newWidth = Math.max(this.quantizedNoteWidth, snappedDx);
 
-                if (newWidth !== this.noteWidth) {
-                    const widthDelta = newWidth - this.noteWidth;
-                    this.x -= widthDelta;
-                    this._reDraw(newWidth);
+                    if (newWidth !== this.noteWidth) {
+                        const widthDelta = newWidth - this.noteWidth;
+                        this.x -= widthDelta;
+                        this._reDraw(newWidth);
+                    }
+                }
+                else if ((!this.resizingRight && !this.resizingLeft) || this.moving) {
+                    this.moving = true;
+                    if (this.moveStartX === undefined) {
+                        this.moveStartX = local.x;
+                    }
+
+                    if (this.moveStartY === undefined) {
+                        this.moveStartY = local.y;
+                    }
+                    const dx = local.x - this.moveStartX;
+                    const dy = local.y - this.moveStartY;
+                    if (Math.abs(dx) >= this.quantizedNoteWidth) {
+                        this.moveStartX = undefined;
+                        var moveDir = dx > 0 ? 1 : -1;
+                        this.x += this.quantizedNoteWidth * moveDir;
+                    }
+
+                    if (Math.abs(dy) >= this.noteHeight) {
+                        this.moveStartY = undefined;
+                        var moveDirY = dy > 0 ? 1 : -1;
+                        this.y += this.noteHeight * moveDirY;
+                    }
                 }
             }
-         }
         });
 
         this.on('pointerdown', (e) => {
@@ -110,12 +136,18 @@ class Note extends Container {
             this.pointerdown = false;
             this.resizingRight = false;
             this.resizingLeft = false;
+            this.moving = false;
+            this.moveStartX = undefined;
+            this.moveStartY = undefined;
         });
 
         this.on('pointerup', (e) => {
             this.pointerdown = false;
             this.resizingRight = false;
             this.resizingLeft = false;
+            this.moving = false;
+            this.moveStartX = undefined;
+            this.moveStartY = undefined;
         });
     }
 }
